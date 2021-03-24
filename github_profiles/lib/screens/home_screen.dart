d70ca45197281961b0422033eb671653a4215dfa
LeomaiaJr <leomaiajr@hotmail.com>
2021-03-23 22:53:12 -0300

    Add basic home screen

--- github_profiles/lib/screens/home_screen.dart
+++ github_profiles/lib/screens/home_screen.dart
@@ -8,14 +8,64 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("GitHub Profiles"),
-      ),
+      backgroundColor: Colors.white,
       body: Center(
         child: Column(
-          children: [],
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: width / 3,
+              child: Image.asset("assets/github-icon.png"),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 25),
+              child: Text(
+                "GitHub Profiles",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25, vertical: 25),
+              child: TextField(
+                style: TextStyle(fontWeight: FontWeight.w400, fontSize: 15),
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                    ),
+                  ),
+                  labelStyle: TextStyle(color: Colors.black),
+                  labelText: "Username",
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+              ),
+              onPressed: () {},
+              child: Text("Search"),
+            )
+          ],
         ),
       ),
     );
