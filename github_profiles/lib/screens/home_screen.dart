a6327855a5efacc9e87a3a2c162f20e9274a1448
LeomaiaJr <leomaiajr@hotmail.com>
2021-03-24 00:09:32 -0300

    Update home screen's theme

--- github_profiles/lib/screens/home_screen.dart
+++ github_profiles/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:github_profiles/widget/app_drawer.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,26 +7,45 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String getImage(BuildContext context) {
+    return Theme.of(context).primaryColor == Colors.black
+        ? "github-icon.png"
+        : "github-icon-white.png";
+  }
+
+  Color getInversedTheme() {
+    return Theme.of(context).primaryColor == Colors.black
+        ? Colors.white
+        : Colors.black;
+  }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: false,
+      backgroundColor: getInversedTheme(),
+      appBar: AppBar(
+        backgroundColor: getInversedTheme(),
+        elevation: 0.0,
+      ),
+      drawer: AppDrawer(),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(
+              height: height / 12,
+            ),
+            SizedBox(
               height: width / 3,
-              child: Image.asset("assets/github-icon.png"),
+              child: Image.asset("assets/${getImage(context)}"),
             ),
             Padding(
               padding: EdgeInsets.only(top: 25),
               child: Text(
                 "GitHub Profiles",
                 style: TextStyle(
-                  color: Colors.black,
                   fontWeight: FontWeight.bold,
                   fontSize: 30,
                 ),
@@ -34,33 +54,36 @@ class _HomeScreenState extends State<HomeScreen> {
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 25, vertical: 25),
               child: TextField(
-                style: TextStyle(fontWeight: FontWeight.w400, fontSize: 15),
-                cursorColor: Colors.black,
+                style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 15,
+                    color: Theme.of(context).primaryColor),
+                cursorColor: Theme.of(context).primaryColor,
                 decoration: InputDecoration(
                   enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(15),
                     borderSide: BorderSide(
-                      color: Colors.black,
+                      color: Theme.of(context).primaryColor,
                     ),
                   ),
                   focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(15),
                     borderSide: BorderSide(
-                      color: Colors.black,
+                      color: Colors.red,
                     ),
                   ),
-                  labelStyle: TextStyle(color: Colors.black),
+                  labelStyle: TextStyle(color: Colors.red),
                   labelText: "Username",
                   prefixIcon: Icon(
                     Icons.search,
-                    color: Colors.black,
+                    color: Colors.red,
                   ),
                 ),
               ),
             ),
             ElevatedButton(
               style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
               ),
               onPressed: () {},
               child: Text("Search"),
