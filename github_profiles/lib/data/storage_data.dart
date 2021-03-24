f085a6f68ab8e9dbd2e9d78f68f509ebc1bd5914
LeomaiaJr <leomaiajr@hotmail.com>
2021-03-24 16:12:34 -0300

    Add comments

--- github_profiles/lib/data/storage_data.dart
+++ github_profiles/lib/data/storage_data.dart
@@ -2,12 +2,12 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemePreference {
   static const THEME_STATUS = "THEMESTATUS";
-
+  //Store the theme
   setDarkTheme(bool value) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setBool(THEME_STATUS, value);
   }
-
+  //Get the theme
   Future<bool> getTheme() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.getBool(THEME_STATUS) ?? false;
--- github_profiles/lib/data/theme_control.dart
+++ github_profiles/lib/data/theme_control.dart
@@ -5,11 +5,11 @@ class DarkThemeProvider with ChangeNotifier {
   ThemePreference darkThemePreference = ThemePreference();
   bool _darkTheme = false;
 
-  bool get darkTheme => _darkTheme;
+  bool get darkTheme => _darkTheme; //getter
 
   set darkTheme(bool value) {
     _darkTheme = value;
-    darkThemePreference.setDarkTheme(value);
-    notifyListeners();
+    darkThemePreference.setDarkTheme(value);//Update Storage for app persistance
+    notifyListeners();//Update Provider Listeners
   }
 }
