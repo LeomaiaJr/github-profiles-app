a5ea9c66f27c3f28c1e4ce32d3f133ac516ebf35
LeomaiaJr <leomaiajr@hotmail.com>
2021-03-24 00:08:16 -0300

    Update theme data

--- github_profiles/lib/data/theme_style.dart
+++ github_profiles/lib/data/theme_style.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
- class Styles {
-   
+class Styles {
   static ThemeData themeData(bool isDarkTheme, BuildContext context) {
     return ThemeData(
+      textTheme: TextTheme(
+        bodyText1: TextStyle(),
+        bodyText2: TextStyle(),
+      ).apply(
+        bodyColor: isDarkTheme ? Colors.white : Colors.black,
+        displayColor: isDarkTheme ? Colors.white : Colors.black,
+      ),
       primarySwatch: Colors.red,
-      primaryColor: isDarkTheme ? Colors.black : Colors.blue,
-
+      primaryColor: isDarkTheme ? Colors.white : Colors.black,
+      canvasColor: isDarkTheme ? Color(0xff212121) : Colors.white,
       backgroundColor: isDarkTheme ? Colors.black : Colors.white,
+      primaryIconTheme: IconThemeData(color: Colors.red),
     );
-    
   }
 }
